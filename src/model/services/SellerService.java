75926c7533b1e96438ee6323be15ba799dc29d69
Ana Caroline Nogueira <carolnogueira13@gmail.com>
2023-02-21 16:50:20 -0300

    SellerList

---
+++ src/model/services/SellerService.java
@@ -0,0 +1,30 @@
+package model.services;
+
+import java.util.List;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerService {
+	
+	private SellerDao dao = DaoFactory.createSellerDao(); // o DaoFactory (fabrica de Daos) que injeta a dependência do SellerDao
+	
+	public List<Seller> findAll(){
+		return dao.findAll();
+	}
+	
+	public void saveOrUpdate(Seller obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+	}
+	
+	public void remove(Seller obj) {
+		dao.deleteById(obj.getId());
+	}
+
+}
